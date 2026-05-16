a4a02eb29fc357c97a5b10c24282a4e0c6f84cbc
AbdullaSami <alattariabdulla@gmail.com>
2026-05-16 20:46:47 +0300

    create department CRUD's function

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -30,4 +30,36 @@ class DepartmentController extends Controller
             return response()->json(['message' => 'An error occurred', 'error' => $e->getMessage()], 500);
         }
     }
+
+    public function show($department)
+    {
+        try {
+            $department = Department::with(['services', 'teams', 'servicesViaTeams'])->where('slug', $department)->first();
+            return response()->json($department);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function update(Request $request, $department)
+    {
+        try {
+            $department = Department::where('slug', $department)->first();
+            $department->update($request->all());
+            return response()->json($department);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function destroy($department)
+    {
+        try {
+            $department = Department::where('slug', $department)->first();
+            $department->delete();
+            return response()->json(['message' => 'Department deleted successfully']);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred', 'error' => $e->getMessage()], 500);
+        }
+    }
 }
