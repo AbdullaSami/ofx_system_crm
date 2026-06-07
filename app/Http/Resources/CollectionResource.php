7aecbed8b6107df15b23a85eb4026c1fd768d592
AbdullaSami <alattariabdulla@gmail.com>
2026-06-07 13:20:30 +0300

    test

--- app/Http/Resources/CollectionResource.php
+++ app/Http/Resources/CollectionResource.php
@@ -15,18 +15,18 @@ class CollectionResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            // 'id'               => $this->id,
-            'contract_number'  => $this->whenLoaded('contract', fn() => $this->contract->contract_number),
-            'employee_name'    => $this->whenLoaded('contract', fn() => $this->contract->employee->name ?? null),
-            'client_name'      => $this->whenLoaded('client', fn() => $this->client->client_name),
-            'amount_due'       => $this->amount_due,
-            'amount_collected' => $this->amount_collected,
-            'due_date'         => $this->due_date,
-            'collection_date'  => $this->collection_date,
-            'status'           => $this->status,
-            'payment_method'   => $this->payment_method,
-            'reference_number' => $this->reference_number,
-            'notes'            => $this->notes,
+            'id'               => $this?->id,
+            'contract_number'  => $this?->whenLoaded('contract', fn() => $this->contract->contract_number),
+            'employee_name'    => $this?->whenLoaded('contract', fn() => $this->contract->employee->name ?? null),
+            'client_name'      => $this?->whenLoaded('client', fn() => $this->client->client_name),
+            'amount_due'       => $this?->amount_due,
+            'amount_collected' => $this?->amount_collected,
+            'due_date'         => $this?->due_date,
+            'collection_date'  => $this?->collection_date,
+            'status'           => $this?->status,
+            'payment_method'   => $this?->payment_method,
+            'reference_number' => $this?->reference_number,
+            'notes'            => $this?->notes,
         ];
     }
 }
