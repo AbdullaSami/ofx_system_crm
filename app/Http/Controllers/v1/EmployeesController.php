1d509c7053d1ed951a989ae8b2bdd0d4419f7d76
Abdulla Sami <100493570+AbdullaSami@users.noreply.github.com>
2026-06-16 17:41:15 +0300

    test

--- app/Http/Controllers/v1/EmployeesController.php
+++ app/Http/Controllers/v1/EmployeesController.php
@@ -24,7 +24,7 @@ class EmployeesController extends Controller
         if (!$employee) {
             return response()->json(['message' => 'Employee not found'], 404);
         }
-        return response()->json(EmployeeResource::collection($employees));
+        return response()->json(EmployeeResource($employees));
     }
 
     public function store(Request $request)
